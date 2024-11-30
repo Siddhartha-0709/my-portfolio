09c26232b26b23ef44220310b6a16998db6ef270
Siddhartha Mukherjee <siddharthamukherjee0709@gmail.com>
2024-12-01 00:35:41 +0530

    add

--- src/components/MyTimeLine.tsx
+++ src/components/MyTimeLine.tsx
@@ -150,19 +150,19 @@ export default function MyTimeLine() {
     },
   ];
   return (
-    <motion.div
-      initial={{ opacity: 0.0, y: 40 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{
-        delay: 0.3,
-        duration: 1.0,
-        ease: "easeInOut",
-      }}>
-      <div className=" w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+    <div className=" w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 1.0,
+          ease: "easeInOut",
+        }}>
         {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <Timeline data={data} />
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,22 +6,23 @@ import { motion } from "framer-motion";
 
 export default function Projects() {
   return (
-    <motion.div
-      initial={{ opacity: 0.0, y: 40 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{
-        delay: 0.3,
-        duration: 0.8,
-        ease: "easeInOut",
-      }}>
-      <div className="w-full h-full flex flex-col bg-black dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative items-center justify-center">
-        <div className="absolute inset-0 w-full h-full bg-black z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+    <div className="w-full h-full flex flex-col bg-black dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative items-center justify-center">
+      <div className="absolute inset-0 w-full h-full bg-black z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}>
         <div className="pt-10 pb-24">
           <TextGenerateEffect words={'Explore My Projects'} size="text-4xl text-white relative z-20 text-center mb-2 font-bold" />
           <TextGenerateEffect words={'Here are the few of the various projects on which I have worked on!'} size="text-center text-xs md:text-xl text-slate-300 relative z-20" />
         </div>
         <ProjectList />
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
+
   );
 }
