7840a12fd466ecf199e31f24de5eedfce03c81ba
Siddhartha Mukherjee <siddharthamukherjee0709@gmail.com>
2025-04-30 11:26:40 +0530

    seo optimisation

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://siddhartha-dev.vercel.app',
+    generateRobotsTxt: true,
+  };
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,17 @@ import HomeHero from "@/components/HomeHero";
 import MyTimeLine from "@/components/MyTimeLine";
 import Projects from "@/components/Projects";
 import Testimonial from "@/components/Testimonial";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Siddhartha | Full Stack Developer</title>
+        <meta name="description" content="Portfolio of Siddhartha, a Full Stack Developer skilled in MERN Stack, DevOps and Cloud." />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://siddhartha-dev.vercel.app/" />
+      </Head>
       <HomeHero />
       <Projects />
       <MyTimeLine />
